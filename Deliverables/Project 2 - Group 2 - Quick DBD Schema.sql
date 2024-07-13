8da1660980d2ceb6df04642bb46deb083e4c398f
Kevin C <kpcarney85@gmail.com>
2024-07-13 17:05:23 -0400

    updates made to deliverables

--- Deliverables/Project 2 - Group 2 - Quick DBD Schema.sql
+++ Deliverables/Project 2 - Group 2 - Quick DBD Schema.sql
@@ -1,5 +1,4 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/ZIeU9K
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
@@ -18,7 +17,7 @@ CREATE TABLE "campaign" (
     "deadline" date   NOT NULL,
     "category_id" varchar(10)   NOT NULL,
     "subcategory_id" varchar(10)   NOT NULL,
-    "last_updated" timestamp   NOT NULL,
+    "last_updated" timestamp  DEFAULT localtimestamp NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
@@ -29,19 +28,28 @@ CREATE TABLE "contacts" (
     "first_name" varchar(50)   NOT NULL,
     "last_name" varchar(50)   NOT NULL,
     "email" varchar(150)   NOT NULL,
-    "last_updated" timestamp   NOT NULL
+    "last_updated" timestamp  DEFAULT localtimestamp NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
 );
 
 CREATE TABLE "category" (
     "category_id" varchar(10)   NOT NULL,
     "category" varchar(20)   NOT NULL,
-    "last_updated" timestamp   NOT NULL
+    "last_updated" timestamp  DEFAULT localtimestamp NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
 );
 
 CREATE TABLE "subcategory" (
     "subcategory_id" varchar(10)   NOT NULL,
     "subcategory" varchar(20)   NOT NULL,
-    "last_updated" timestamp   NOT NULL
+    "last_updated" timestamp  DEFAULT localtimestamp NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
 );
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
